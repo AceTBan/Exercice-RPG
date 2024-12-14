62b32832e48558f42cd2acc4b9afac48c173dc9f
AceTBan <esteban_david@ymail.com>
2024-12-14 20:30:03 +0100

    amélioration app.js (affiche pv restants)

--- RPG/app.js
+++ RPG/app.js
@@ -18,6 +18,13 @@ document.addEventListener('DOMContentLoaded',function() {
         if (malik && valentin) {
             resume.innerHTML += malik.patate(valentin);
             resume.innerHTML += valentin.patate(malik);
+			resume.innerHTML += '<p>' + malik.nom + ' : ' + malik.sante + ' points de vie restants</p>';
+            resume.innerHTML += '<p>' + valentin.nom + ' : ' + valentin.sante + ' points de vie restants</p>';
+
+            if (malik.sante <= 0 || valentin.sante <= 0) {
+                attaque.disabled = true; // Désactivation du bouton "attaque" lorsque le combat est terminé
+                resume.innerHTML += '<p>Le combat est terminé !</p>';
+            }
         }
     });
 });
